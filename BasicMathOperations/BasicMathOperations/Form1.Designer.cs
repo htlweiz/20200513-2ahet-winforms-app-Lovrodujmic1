b0d5befa8f47aea8faf2d7f581ecc904c3ff60f6
Lovrodujmic1 <58843880+Lovrodujmic1@users.noreply.github.com>
2020-05-15 11:39:00 +0200

    Add files via upload

---
+++ BasicMathOperations/BasicMathOperations/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace BasicMathOperations
+{
+    partial class titel
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.LblNumber2 = new System.Windows.Forms.Label();
+            this.LblNumber3 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.LblNumber4 = new System.Windows.Forms.Label();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnE = new System.Windows.Forms.Button();
+            this.btnF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Location = new System.Drawing.Point(80, 23);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(699, 66);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Einfache Rechenoperationen";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitle.UseCompatibleTextRendering = true;
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Location = new System.Drawing.Point(22, 147);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(82, 31);
+            this.lblNumber1.TabIndex = 2;
+            this.lblNumber1.Text = "Zahl1";
+            this.lblNumber1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LblNumber2
+            // 
+            this.LblNumber2.AutoSize = true;
+            this.LblNumber2.Location = new System.Drawing.Point(22, 223);
+            this.LblNumber2.Name = "LblNumber2";
+            this.LblNumber2.Size = new System.Drawing.Size(82, 31);
+            this.LblNumber2.TabIndex = 3;
+            this.LblNumber2.Text = "Zahl2";
+            this.LblNumber2.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // LblNumber3
+            // 
+            this.LblNumber3.AutoSize = true;
+            this.LblNumber3.Location = new System.Drawing.Point(12, 295);
+            this.LblNumber3.Name = "LblNumber3";
+            this.LblNumber3.Size = new System.Drawing.Size(121, 31);
+            this.LblNumber3.TabIndex = 4;
+            this.LblNumber3.Text = "Ergebnis";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(162, 144);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(175, 38);
+            this.txt1.TabIndex = 5;
+            this.txt1.Text = "0";
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(162, 223);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(175, 38);
+            this.txt2.TabIndex = 6;
+            this.txt2.Text = "0";
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // LblNumber4
+            // 
+            this.LblNumber4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblNumber4.Location = new System.Drawing.Point(162, 295);
+            this.LblNumber4.Name = "LblNumber4";
+            this.LblNumber4.Size = new System.Drawing.Size(175, 41);
+            this.LblNumber4.TabIndex = 8;
+            this.LblNumber4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LblNumber4.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(394, 121);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(69, 67);
+            this.btnA.TabIndex = 9;
+            this.btnA.Text = "+";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(394, 205);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(69, 67);
+            this.btnB.TabIndex = 10;
+            this.btnB.Text = "*";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(469, 121);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(69, 67);
+            this.btnC.TabIndex = 11;
+            this.btnC.Text = "-";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(469, 205);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(69, 67);
+            this.btnD.TabIndex = 12;
+            this.btnD.Text = "/";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // btnE
+            // 
+            this.btnE.Location = new System.Drawing.Point(548, 121);
+            this.btnE.Name = "btnE";
+            this.btnE.Size = new System.Drawing.Size(69, 67);
+            this.btnE.TabIndex = 13;
+            this.btnE.Text = "b√a";
+            this.btnE.UseVisualStyleBackColor = true;
+            this.btnE.Click += new System.EventHandler(this.btnE_Click);
+            // 
+            // btnF
+            // 
+            this.btnF.Location = new System.Drawing.Point(548, 205);
+            this.btnF.Name = "btnF";
+            this.btnF.Size = new System.Drawing.Size(69, 67);
+            this.btnF.TabIndex = 14;
+            this.btnF.Text = "a^b";
+            this.btnF.UseVisualStyleBackColor = true;
+            this.btnF.Click += new System.EventHandler(this.btnF_Click);
+            // 
+            // titel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 379);
+            this.Controls.Add(this.btnF);
+            this.Controls.Add(this.btnE);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.LblNumber4);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.LblNumber3);
+            this.Controls.Add(this.LblNumber2);
+            this.Controls.Add(this.lblNumber1);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "titel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Einfacher Rechner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.Label LblNumber2;
+        private System.Windows.Forms.Label LblNumber3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label LblNumber4;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnE;
+        private System.Windows.Forms.Button btnF;
+    }
+}
+
---
+++ BasicMathOperations/BasicMathOperations/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicMathOperations
+{
+    public partial class titel : Form
+    {
+        public titel()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            int number1, number2, result;
+            Int32.TryParse(txt1.Text, out number1);
+            Int32.TryParse(txt2.Text, out number2);
+            result = number1 + number2;
+            LblNumber4.Text = Convert.ToString(result);
+
+            try
+            {
+                number1 = Convert.ToInt32(txt1.Text);
+                number2 = Convert.ToInt32(txt2.Text);
+                result = number1 + number2;
+                LblNumber4.Text = Convert.ToString(result);
+            }
+            catch
+            {
+                LblNumber4.Text = "Fehler";
+                MessageBox.Show("Bitte Schreib Sie nur Zahlen ein!","Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt1.Focus();
+                txt1.SelectAll();
+            }
+                }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnF_Click(object sender, EventArgs e)
+        {
+
+            int number1, number2;
+            double result;
+            Int32.TryParse(txt1.Text, out number1);
+            Int32.TryParse(txt2.Text, out number2);
+            result = (double)number1 / number2;
+            LblNumber4.Text = Convert.ToString(result);
+
+            try
+            {
+
+                number1 = Convert.ToInt32(txt1.Text);
+                number2 = Convert.ToInt32(txt2.Text);
+
+                if (number1 == 0 || number2 == 0)
+                {
+                    LblNumber4.Text = "Fehler";
+                    MessageBox.Show("Eingabefehler", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt1.Focus();
+                    txt1.SelectAll();
+                }
+                else
+                {
+                    result = Math.Pow(number1, number2);
+                    LblNumber4.Text = Convert.ToString(result);
+                }
+            }
+            catch
+            {
+                LblNumber4.Text = "Fehler";
+                MessageBox.Show("Eingabefehler", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt1.Focus();
+                txt1.SelectAll();
+            }
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            {
+                int number1, number2, result;
+                Int32.TryParse(txt1.Text, out number1);
+                Int32.TryParse(txt2.Text, out number2);
+                result = number1 - number2;
+                LblNumber4.Text = Convert.ToString(result);
+
+                try
+                {
+                    number1 = Convert.ToInt32(txt1.Text);
+                    number2 = Convert.ToInt32(txt2.Text);
+                    result = number1 - number2;
+                    LblNumber4.Text = Convert.ToString(result);
+                }
+                catch
+                {
+                    LblNumber4.Text = "Fehler";
+                    MessageBox.Show("Bitte Schreib Sie nur Zahlen ein!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt1.Focus();
+                    txt1.SelectAll();
+                }
+            }
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            {
+                int number1, number2;
+                double result;
+                Int32.TryParse(txt1.Text, out number1);
+                Int32.TryParse(txt2.Text, out number2);
+                result = number1 * number2;
+                LblNumber4.Text = Convert.ToString(result);
+
+                try
+                {
+                    number1 = Convert.ToInt32(txt1.Text);
+                    number2 = Convert.ToInt32(txt2.Text);
+                    result = number1 * number2;
+                    LblNumber4.Text = Convert.ToString(result);
+                }
+                catch
+                {
+                    LblNumber4.Text = "Fehler";
+                    MessageBox.Show("Eingabefehler", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt1.Focus();
+                    txt1.SelectAll();
+                }
+            }
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            int number1, number2;
+            double result;
+            Int32.TryParse(txt1.Text, out number1);
+            Int32.TryParse(txt2.Text, out number2);
+            result = (double) number1 / number2;
+            LblNumber4.Text = Convert.ToString(result);
+
+            try
+            {
+                
+                number1 = Convert.ToInt32(txt1.Text);
+                number2 = Convert.ToInt32(txt2.Text);
+                if (number2 == 0)
+                {
+                    LblNumber4.Text = "Fehler";
+                    MessageBox.Show("Division durch 0!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt1.Focus();
+                    txt1.SelectAll();
+                }
+                else
+                {
+                    result = (double)number1 / number2;
+                    LblNumber4.Text = Convert.ToString(result);
+
+                }
+            }
+            catch
+            {
+                LblNumber4.Text = "Fehler";
+                MessageBox.Show("Eingabefehler", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt1.Focus();
+                txt1.SelectAll();
+            }
+        }
+
+        private void btnE_Click(object sender, EventArgs e)
+        {
+            
+                int number1, number2;
+                double result;
+                Int32.TryParse(txt1.Text, out number1);
+                Int32.TryParse(txt2.Text, out number2);
+                result = (double)number1 / number2;
+                LblNumber4.Text = Convert.ToString(result);
+
+                try
+                {
+
+                    number1 = Convert.ToInt32(txt1.Text);
+                    number2 = Convert.ToInt32(txt2.Text);
+                    
+                    if (number1==0 || number2==0)
+                    {
+                        LblNumber4.Text = "Fehler";
+                        MessageBox.Show("Eingabefehler", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txt1.Focus();
+                        txt1.SelectAll();
+                    }
+                    else
+                    {
+                    result = Math.Pow(number1, 1.0 / number2);
+                    LblNumber4.Text = Convert.ToString(result);
+                    }
+                }
+                catch
+                {
+                    LblNumber4.Text = "Fehler";
+                    MessageBox.Show("Eingabefehler", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt1.Focus();
+                    txt1.SelectAll();
+                }
+
+                
+
+
+
+        }
+    }
+}
